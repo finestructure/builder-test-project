9b00254e8b4820f51a248f8330e4e07e5b9c6db6
Sven A. Schmidt <sas@finestructure.co>
2024-10-19 12:26:52 +0200

    Rename things, add second target

--- Package.swift
+++ Package.swift
@@ -1,23 +1,15 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "builder-test-project",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "builder-test-project",
-            targets: ["builder-test-project"]),
+        .library(name: "builder-test-project", targets: ["target-1", "target-2"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "builder-test-project"),
-        .testTarget(
-            name: "builder-test-projectTests",
-            dependencies: ["builder-test-project"]),
+        .target(name: "target-1"),
+        .target(name: "target-2"),
+        .testTarget(name: "test-target", dependencies: ["target-1", "target-2"]),
     ]
 )
